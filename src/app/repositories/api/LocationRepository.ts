fdd507035b5cf1bbab1518ce1b14c93b2f97ef52
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-14 14:09:42 -0300

    refactor: Update LocationRepository to use static method for getLocations

--- src/app/repositories/api/LocationRepository.ts
+++ src/app/repositories/api/LocationRepository.ts
@@ -1,12 +1,10 @@
 import { BaseRepository } from './shared/BaseRepository';
 
-export class LocationRepositoryClass extends BaseRepository {
-  async getLocations(params: TGetLocationsParams) {
+export class LocationRepository extends BaseRepository {
+  public static async getLocations(params: TGetLocationsParams) {
     return super.getAll<TLocation>({
       url: 'local',
       params,
     });
   }
 }
-
-export const LocationRepository = new LocationRepositoryClass();
