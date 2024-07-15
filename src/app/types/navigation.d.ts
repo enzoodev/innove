288d6291b84a74ea9fce8d3087a98870e6af0d52
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-14 22:40:31 -0300

    feat: Add Settings and UpdatePassword screens to AppStackParamsList
    The Settings and UpdatePassword screens were added to the AppStackParamsList in order to provide navigation functionality for these features.

--- src/app/types/navigation.d.ts
+++ src/app/types/navigation.d.ts
@@ -9,10 +9,13 @@ type TChecklistRouteParams = {
 
 type AppStackParamsList = {
   Home: undefined;
+  Settings: undefined;
+  UpdatePassword: undefined;
   ExecutionDetails: TExecutionDetailsRouteParams;
   Checklist: TChecklistRouteParams;
 };
 
 type AuthStackParamsList = {
   Login: undefined;
+  RecoverAccount: undefined;
 };
