364f0af47e3a69fa59019bb5b15a22dc4da73f02
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-14 22:42:00 -0300

    feat: Add UpdatePassword and Settings screens
    This commit adds the UpdatePassword and Settings screens to the app routes. The UpdatePassword screen allows users to update their password, while the Settings screen allows users to configure their account settings. These features enhance the app by providing a secure way for users to update their password and a dedicated settings section for account customization.

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from '@/screens/app/Home';
+import { Settings } from '@/screens/app/Settings';
 import { ExecutionDetails } from '@/screens/app/Home/ExecutionDetails';
+import { UpdatePassword } from '@/screens/app/UpdatePassword';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -8,6 +10,8 @@ export const AppRoutes = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={Home} />
+      <Screen name="UpdatePassword" component={UpdatePassword} />
+      <Screen name="Settings" component={Settings} />
       <Screen name="ExecutionDetails" component={ExecutionDetails} />
     </Navigator>
   );
