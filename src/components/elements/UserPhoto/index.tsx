3583f8471eba562e28c048e49619743131494fd9
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-14 22:41:06 -0300

    feat: Add UserPhoto component
    This commit adds the UserPhoto component, which displays the user's photo as an Image component. The component accepts a size prop to customize the width and height of the image. The photo URL is obtained from the authenticated user's client_logo_icon property and formatted using the PhotoFormatter utility. This component improves the user experience by displaying the user's photo in the app.

---
+++ src/components/elements/UserPhoto/index.tsx
@@ -0,0 +1,28 @@
+import { memo } from 'react';
+import { Image } from 'react-native';
+
+import { PhotoFormatter } from '@/app/utils/PhotoFormatter';
+import { useAuth } from '@/hooks/useAuth';
+
+type Props = {
+  size: number;
+};
+
+export const UserPhoto = memo(({ size }: Props) => {
+  const { auth } = useAuth();
+
+  if (!auth) {
+    return null;
+  }
+
+  const uri = PhotoFormatter.formatUri(auth.client_logo_icon);
+
+  return (
+    <Image
+      width={size}
+      height={size}
+      borderRadius={size / 2}
+      source={{ uri }}
+    />
+  );
+});
