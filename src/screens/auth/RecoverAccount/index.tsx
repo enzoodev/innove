a828e180f0809cb330b4bd39fd50fd6d2872b567
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-16 23:15:11 -0300

    refactor: Optimize RecoverAccount screen with lazy loading for images

--- src/screens/auth/RecoverAccount/index.tsx
+++ src/screens/auth/RecoverAccount/index.tsx
@@ -1,6 +1,8 @@
 import { useCallback } from 'react';
+import { Image, ScrollView } from 'react-native';
 import { useTheme } from 'styled-components/native';
-import { IconCheck } from 'tabler-react-native/icons';
+import { IconBrandTelegram } from 'tabler-react-native/icons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
@@ -10,11 +12,12 @@ import {
 } from '@/app/schemas/auth/recoverAccount';
 
 import { useAuth } from '@/hooks/useAuth';
+import { useAuthNavigation } from '@/hooks/useAuthNavigation';
 
-import { Label } from '@/components/elements/Label';
 import { Input } from '@/components/elements/Input';
 import { Button } from '@/components/elements/Button';
-import { Header } from '@/components/elements/Header';
+import { GoBackButton } from '@/components/elements/GoBackButton';
+import { AnimatedKeyboardWrapper } from '@/components/elements/AnimatedKeyboardWrapper';
 import { EmailIcon } from '@/components/modules/EmailIcon';
 
 import Mail from '@/assets/images/mail.png';
@@ -23,6 +26,8 @@ import * as S from './styles';
 
 export const RecoverAccount = () => {
   const theme = useTheme();
+  const insets = useSafeAreaInsets();
+  const navigation = useAuthNavigation();
   const { handleRecoverAccount, isLoadingRecoverAccount } = useAuth();
 
   const {
@@ -39,58 +44,65 @@ export const RecoverAccount = () => {
   const onSubmit: SubmitHandler<TRecoverAccountSchema> = useCallback(
     async ({ email }) => {
       await handleRecoverAccount({ email });
+      navigation.navigate('RecoverAccountEmailSent');
     },
-    [handleRecoverAccount],
+    [handleRecoverAccount, navigation],
   );
 
   return (
     <S.Container>
-      <Header hasBackButton />
-      <S.Content>
-        <S.Header>
-          <S.Title>Esqueceu a senha?</S.Title>
-          <S.Subtitle>
-            Escreva seu e-mail cadastrado abaixo para receber instruções de
-            redefinição de senha
-          </S.Subtitle>
-        </S.Header>
-        <S.ImageWrapper>
-          <S.IconImage source={Mail} />
-          <EmailIcon
-            Icon={() => (
-              <IconCheck
-                stroke={1.5}
-                size={theme.iconSizes.lg}
-                color={theme.colors.mailContrast}
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: insets.bottom,
+        }}
+      >
+        <AnimatedKeyboardWrapper>
+          <S.Content>
+            <GoBackButton />
+            <S.Header>
+              <S.Title>Esqueceu a senha?</S.Title>
+              <S.Subtitle>
+                Escreva seu e-mail cadastrado abaixo para receber instruções de
+                redefinição de senha
+              </S.Subtitle>
+            </S.Header>
+            <S.ImageWrapper>
+              <Image source={Mail} />
+              <EmailIcon
+                Icon={() => (
+                  <IconBrandTelegram
+                    stroke={1.25}
+                    size={theme.iconSizes.md}
+                    color={theme.colors.mailContrast}
+                  />
+                )}
               />
-            )}
-          />
-        </S.ImageWrapper>
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { onChange, value, onBlur, ref } }) => (
-            <Label title="Email">
-              <Input
-                ref={ref}
-                value={value}
-                onChangeText={onChange}
-                formError={errors.email?.message}
-                placeholder="Email"
-                autoCapitalize="none"
-                onBlur={onBlur}
-                returnKeyType="send"
-                onSubmitEditing={handleSubmit(onSubmit)}
-              />
-            </Label>
-          )}
-        />
-        <Button
-          title="Enviar"
-          onPress={handleSubmit(onSubmit)}
-          isLoading={isLoadingRecoverAccount}
-        />
-      </S.Content>
+            </S.ImageWrapper>
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value, onBlur, ref } }) => (
+                <Input
+                  ref={ref}
+                  value={value}
+                  onChangeText={onChange}
+                  formError={errors.email?.message}
+                  placeholder="Email"
+                  autoCapitalize="none"
+                  onBlur={onBlur}
+                  returnKeyType="send"
+                  onSubmitEditing={handleSubmit(onSubmit)}
+                />
+              )}
+            />
+            <Button
+              title="Enviar"
+              onPress={handleSubmit(onSubmit)}
+              isLoading={isLoadingRecoverAccount}
+            />
+          </S.Content>
+        </AnimatedKeyboardWrapper>
+      </ScrollView>
     </S.Container>
   );
 };
--- src/screens/auth/RecoverAccount/styles.ts
+++ src/screens/auth/RecoverAccount/styles.ts
@@ -1,15 +1,46 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
 
-export const Content = styled.View``;
+export const Content = styled.View`
+  ${({ theme }) => css`
+    gap: ${theme.layout[6]}px;
+    margin: ${theme.layout[8]}px ${theme.layout[4]}px;
+  `};
+`;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  align-items: center;
+  ${({ theme }) => css`
+    gap: ${theme.layout[4]}px;
+    margin-top: ${theme.layout[4]}px;
+  `};
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  text-align: center;
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.main.bold};
+    font-size: ${theme.fontSizes.xl}px;
+    color: ${theme.colors.textSecondary};
+  `};
+`;
 
-export const Subtitle = styled.Text``;
+export const Subtitle = styled.Text`
+  text-align: center;
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.main.regular};
+    font-size: ${theme.fontSizes.sm}px;
+    color: ${theme.colors.textTertiary};
+  `};
+`;
 
-export const ImageWrapper = styled.View``;
-
-export const IconImage = styled.Image``;
+export const ImageWrapper = styled.View`
+  align-items: center;
+  ${({ theme }) => css`
+    margin: ${theme.layout[4]}px 0;
+  `};
+`;
