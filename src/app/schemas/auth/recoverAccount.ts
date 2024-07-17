15f4e5e19a8c3bf5618e3ef38161d75855b95014
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-16 23:14:34 -0300

    feat: Update email validation in recoverAccountSchema

--- src/app/schemas/auth/recoverAccount.ts
+++ src/app/schemas/auth/recoverAccount.ts
@@ -1,7 +1,7 @@
 import { z, TypeOf } from 'zod';
 
 export const recoverAccountSchema = z.object({
-  email: z.string().email('Email inválido.').min(1, 'Informe o email.'),
+  email: z.string().min(1, 'Informe o email.').email('Email inválido.'),
 });
 
 export type TRecoverAccountSchema = TypeOf<typeof recoverAccountSchema>;
