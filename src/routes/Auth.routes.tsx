728c710e429ec8bea109200597f049908249084c
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-17 09:14:10 -0300

    feat: Add RecoverAccountEmailSent screen to AuthRoutes

--- src/routes/Auth.routes.tsx
+++ src/routes/Auth.routes.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from '@/screens/auth/Login';
 import { RecoverAccount } from '@/screens/auth/RecoverAccount';
+import { RecoverAccountEmailSent } from '@/screens/auth/RecoverAccountEmailSent';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -9,6 +10,10 @@ export const AuthRoutes = () => {
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Login" component={Login} />
       <Screen name="RecoverAccount" component={RecoverAccount} />
+      <Screen
+        name="RecoverAccountEmailSent"
+        component={RecoverAccountEmailSent}
+      />
     </Navigator>
   );
 };
