60899eb47854053efabf5cee0510be1ac79f40ff
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-16 23:14:54 -0300

    feat: Add email icon to the EmailIcon component

--- src/components/modules/EmailIcon/styles.ts
+++ src/components/modules/EmailIcon/styles.ts
@@ -1,3 +1,15 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  ${({ theme }) => css`
+    bottom: ${theme.layout[3]}px;
+    right: ${theme.layout[18]}px;
+    width: ${theme.layout[10]}px;
+    height: ${theme.layout[10]}px;
+    border-radius: ${theme.border.radius.full}px;
+    background-color: ${theme.colors.mail};
+  `};
+`;
