30284520a493526d4435d9c58e3920c36cdc98a6
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-16 23:15:24 -0300

    refactor: Add RecoverAccount screen to AuthRoutes

--- src/routes/Auth.routes.tsx
+++ src/routes/Auth.routes.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from '@/screens/auth/Login';
+import { RecoverAccount } from '@/screens/auth/RecoverAccount';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -7,6 +8,7 @@ export const AuthRoutes = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Login" component={Login} />
+      <Screen name="RecoverAccount" component={RecoverAccount} />
     </Navigator>
   );
 };
