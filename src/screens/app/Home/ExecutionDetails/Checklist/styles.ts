28ca8b8647b07c72a2c7d6552eab5b6930e67706
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-21 16:32:11 -0300

    refactor: Update Checklist screen styles

---
+++ src/screens/app/Home/ExecutionDetails/Checklist/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  margin: 0 ${({ theme }) => theme.layout[4]}px;
+`;
