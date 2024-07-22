37a9330e0d023a1c0ebfd65e63f9e95f56d366db
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-07-22 16:51:36 -0300

    refactor: Update Question component to use more descriptive prop names

--- src/components/modules/Question/index.tsx
+++ src/components/modules/Question/index.tsx
@@ -5,30 +5,30 @@ import { useTheme } from 'styled-components/native';
 import * as S from './styles';
 
 type Props = {
-  questionName: string;
-  questionResponse: string;
+  name: string;
+  response: string;
   answers: TAnswerType[];
   onRespond: (answer: string) => void;
 };
 
 export const Question = memo(
-  ({ questionName, questionResponse, answers, onRespond }: Props) => {
+  ({ name, response, answers, onRespond }: Props) => {
     const theme = useTheme();
 
     return (
       <S.Container>
-        <S.Title>{questionName}</S.Title>
+        <S.Title>{name}</S.Title>
         <S.ResponsesContainer>
           {answers.map(answer => (
             <S.Response key={answer.id}>
-              <S.ResponseText isSelected={questionResponse === answer.id}>
+              <S.ResponseText isSelected={response === answer.id}>
                 {answer.nome}
               </S.ResponseText>
               <S.ResponseButton
-                isSelected={questionResponse === answer.id}
+                isSelected={response === answer.id}
                 onPress={() => onRespond(answer.id)}
               >
-                {questionResponse === answer.id && (
+                {response === answer.id && (
                   <IconX
                     stroke={1.5}
                     size={theme.iconSizes.xs}
