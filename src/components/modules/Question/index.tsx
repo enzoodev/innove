08295d7564d64f2226da7ea9a18a06f91149f619
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-07-25 19:09:20 -0300

    refactor: Update Question component to include control, errors, sectionIndex, questionIndex, question, answers, onRespond, onSetPhoto, and onDeletePhoto props

--- src/components/modules/Question/index.tsx
+++ src/components/modules/Question/index.tsx
@@ -1,34 +1,60 @@
 import { memo } from 'react';
 import { IconX } from 'tabler-react-native/icons';
 import { useTheme } from 'styled-components/native';
+import { Control, Controller, FieldErrors } from 'react-hook-form';
+
+import {
+  TSaveChecklistQuestionSchema,
+  TSaveChecklistSchema,
+} from '@/app/schemas/checklist/saveChecklistSchema';
+import { ListSeparators } from '@/app/utils/ListSeparators';
+
+import { Label } from '@/components/elements/Label';
+import { Input } from '@/components/elements/Input';
+import { QuestionPhoto } from '@/components/modules/QuestionPhoto';
 
 import * as S from './styles';
 
 type Props = {
-  name: string;
-  response: string;
+  control: Control<TSaveChecklistSchema>;
+  errors: FieldErrors<TSaveChecklistSchema>;
+  sectionIndex: number;
+  questionIndex: number;
+  question: TSaveChecklistQuestionSchema;
   answers: TAnswerType[];
   onRespond: (answer: string) => void;
+  onSetPhoto: (photoUri: string) => void;
+  onDeletePhoto: (photoIndex: number) => void;
 };
 
 export const Question = memo(
-  ({ name, response, answers, onRespond }: Props) => {
+  ({
+    control,
+    errors,
+    sectionIndex,
+    questionIndex,
+    question,
+    answers,
+    onRespond,
+    onSetPhoto,
+    onDeletePhoto,
+  }: Props) => {
     const theme = useTheme();
 
     return (
       <S.Container>
-        <S.Title>{name}</S.Title>
+        <S.Title>{question.name}</S.Title>
         <S.ResponsesContainer>
           {answers.map(answer => (
             <S.Response key={answer.id}>
-              <S.ResponseText isSelected={response === answer.id}>
+              <S.ResponseText isSelected={question.idanswerstype === answer.id}>
                 {answer.nome}
               </S.ResponseText>
               <S.ResponseButton
-                isSelected={response === answer.id}
+                isSelected={question.idanswerstype === answer.id}
                 onPress={() => onRespond(answer.id)}
               >
-                {response === answer.id && (
+                {question.idanswerstype === answer.id && (
                   <IconX
                     stroke={1.5}
                     size={theme.iconSizes.xs}
@@ -39,6 +65,42 @@ export const Question = memo(
             </S.Response>
           ))}
         </S.ResponsesContainer>
+        {question.idanswerstype === '2' && (
+          <S.JustificationsContainer>
+            <Controller
+              control={control}
+              name={`sections.${sectionIndex}.questions.${questionIndex}.comments`}
+              render={({ field: { onChange, value, onBlur, ref } }) => (
+                <Label title="Justificativa">
+                  <Input
+                    ref={ref}
+                    value={value}
+                    onChangeText={onChange}
+                    formError={
+                      errors.sections?.[sectionIndex]?.questions?.[
+                        questionIndex
+                      ]?.comments?.message
+                    }
+                    placeholder="Justificativa"
+                    onBlur={onBlur}
+                  />
+                </Label>
+              )}
+            />
+            <S.PhotosWrapper horizontal showsHorizontalScrollIndicator={false}>
+              {question.photos.map((photo, index, array) => (
+                <QuestionPhoto
+                  key={photo.photoUri}
+                  uri={photo.photoUri}
+                  index={index}
+                  isLastItem={ListSeparators.getIsLastItem(index, array)}
+                  setPhoto={onSetPhoto}
+                  deletePhoto={() => onDeletePhoto(index)}
+                />
+              ))}
+            </S.PhotosWrapper>
+          </S.JustificationsContainer>
+        )}
       </S.Container>
     );
   },
