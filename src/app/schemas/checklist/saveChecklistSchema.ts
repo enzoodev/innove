052653c6d3070cb703d7f8e4d7eec4d6d9b34d6d
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-07-25 19:08:56 -0300

    refactor: Update saveChecklistSchema to include nullable photoUri in questionSchema

--- src/app/schemas/checklist/saveChecklistSchema.ts
+++ src/app/schemas/checklist/saveChecklistSchema.ts
@@ -4,50 +4,49 @@ const photoSchema = z.object({
   executionId: z.string(),
   checklistId: z.string(),
   questionId: z.string().optional(),
-  counter: z.number().optional(),
-  photoUri: z.string(),
+  photoUri: z.string().nullable(),
 });
+
+const questionSchema = z
+  .object({
+    idquestion: z.string(),
+    idanswerstype: z.string(),
+    idclassification: z.string().optional(),
+    comments: z.string().optional(),
+    name: z.string(),
+    photos: z.array(photoSchema),
+  })
+  .refine(
+    data => {
+      if (data.idanswerstype === '1') {
+        return data.idclassification !== undefined;
+      }
+      return true;
+    },
+    {
+      message:
+        'Informe a classificação para a questão quando a resposta for não conforme.',
+      path: ['idclassification'],
+    },
+  )
+  .refine(
+    data => {
+      if (data.idanswerstype === '1') {
+        return data.comments !== undefined;
+      }
+      return true;
+    },
+    {
+      message:
+        'Escreva um comentário para a questão quando a resposta for não conforme.',
+      path: ['comments'],
+    },
+  );
 
 export const sectionSchema = z.object({
   subtitle: z.string(),
   isOpen: z.boolean(),
-  questions: z.array(
-    z
-      .object({
-        idquestion: z.string(),
-        idanswerstype: z.string(),
-        idclassification: z.string().optional(),
-        comments: z.string().optional(),
-        name: z.string(),
-        photos: z.array(photoSchema),
-      })
-      .refine(
-        data => {
-          if (data.idanswerstype === '1') {
-            return data.idclassification !== undefined;
-          }
-          return true;
-        },
-        {
-          message:
-            'Informe a classificação para a questão quando a resposta for não conforme.',
-          path: ['idclassification'],
-        },
-      )
-      .refine(
-        data => {
-          if (data.idanswerstype === '1') {
-            return data.comments !== undefined;
-          }
-          return true;
-        },
-        {
-          message:
-            'Escreva um comentário para a questão quando a resposta for não conforme.',
-          path: ['comments'],
-        },
-      ),
-  ),
+  questions: z.array(questionSchema),
 });
 
 export const saveChecklistSchema = z.object({
@@ -56,6 +55,8 @@ export const saveChecklistSchema = z.object({
 });
 
 export type TSaveChecklistPhotoSchema = TypeOf<typeof photoSchema>;
+
+export type TSaveChecklistQuestionSchema = TypeOf<typeof questionSchema>;
 
 export type TSaveChecklistSectionSchema = TypeOf<typeof sectionSchema>;
 
