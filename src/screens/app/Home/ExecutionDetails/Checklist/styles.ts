591bc417ab12df3ac21f04c411c709cc5732c986
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-25 23:31:47 -0300

    refactor: Update ChecklistSection component to include ButtonWrapper for better layout spacing

--- src/screens/app/Home/ExecutionDetails/Checklist/styles.ts
+++ src/screens/app/Home/ExecutionDetails/Checklist/styles.ts
@@ -6,4 +6,9 @@ export const Container = styled.View`
 
 export const Content = styled.View`
   flex: 1;
+  gap: ${({ theme }) => theme.layout[4]}px;
+`;
+
+export const ButtonWrapper = styled.View`
+  margin: 0 ${({ theme }) => theme.layout[4]}px;
 `;
