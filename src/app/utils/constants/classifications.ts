d96f136ecdfb2e29e07acdc51bfa1725efae440a
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-25 23:31:01 -0300

    refactor: Update classifications constant to include "Baixo", "Médio", and "Alto" labels

---
+++ src/app/utils/constants/classifications.ts
@@ -0,0 +1,14 @@
+export const classifications = [
+  {
+    value: '1',
+    label: 'Baixo',
+  },
+  {
+    value: '2',
+    label: 'Médio',
+  },
+  {
+    value: '3',
+    label: 'Alto',
+  },
+];
