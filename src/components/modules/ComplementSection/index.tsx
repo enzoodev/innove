588267200fb7468e9a79ad1991022d65fbfb9271
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-26 15:31:31 -0300

    refactor: Update components to include photo actions and improve error handling

--- src/components/modules/ComplementSection/index.tsx
+++ src/components/modules/ComplementSection/index.tsx
@@ -1,16 +1,25 @@
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
 import { useTheme } from 'styled-components/native';
 import {
   IconCamera,
+  IconCameraOff,
   IconChevronDown,
   IconTrash,
 } from 'tabler-react-native/icons';
+
+import { useAppNavigation } from '@/hooks/shared/useAppNavigation';
+import {
+  DeleteComplementPhotoParams,
+  SetComplementPhotoParams,
+} from '@/hooks/api/useFormChecklist';
 
 import { TSaveChecklistPhotoSchema } from '@/app/schemas/checklist/saveChecklistSchema';
 import { photosQuantityPerSection } from '@/app/utils/constants/photosQuantityPerSection';
 import { ListSeparators } from '@/app/utils/ListSeparators';
 
 import { IconButton } from '@/components/elements/IconButton';
+import { CollapsableContainer } from '@/components/elements/CollapsableContainer';
 
 import * as S from './styles';
 
@@ -18,80 +27,129 @@ type Props = {
   photos: Array<TSaveChecklistPhotoSchema>;
   isOpen: boolean;
   onOpenSection: () => void;
+  onSetPhoto: ({ photoUri, photoIndex }: SetComplementPhotoParams) => void;
+  onDeletePhoto: ({ photoIndex }: DeleteComplementPhotoParams) => void;
 };
 
 export const ComplementSection = memo(
-  ({ photos, isOpen, onOpenSection }: Props) => {
+  ({ photos, isOpen, onOpenSection, onSetPhoto, onDeletePhoto }: Props) => {
     const theme = useTheme();
+    const navigation = useAppNavigation();
+
+    const handleTakePhoto = useCallback(
+      (setPhoto: (uri: string) => void) => {
+        navigation.navigate('TakeChecklistPhoto', {
+          setPhoto,
+        });
+      },
+      [navigation],
+    );
 
     return (
-      <S.Container isOpen={isOpen}>
+      <S.Container>
         {isOpen ? (
-          <S.PhotosScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {photos.map((item, index, array) => (
-              <S.ImageWrapper
-                isLastItem={ListSeparators.getIsLastItem(index, array)}
-              >
-                <S.Header isOpen>
-                  <S.Title isOpen>
-                    Fotos complementares {index + 1}/
-                    {photosQuantityPerSection.complement}
-                  </S.Title>
-                  {isOpen ? (
-                    <S.ButtonsWrapper>
-                      <IconButton
-                        onPress={}
-                        Icon={() => (
-                          <IconCamera
-                            stroke={1.5}
-                            size={theme.iconSizes.sm}
-                            color={theme.colors.textSecondary}
-                          />
-                        )}
-                      />
-                      <IconButton
-                        disabled={!item.photoUri}
-                        isDisabled={!item.photoUri}
-                        onPress={}
-                        Icon={() => (
-                          <IconTrash
-                            stroke={1.5}
-                            size={theme.iconSizes.sm}
-                            color={theme.colors.textSecondary}
-                          />
-                        )}
-                      />
-                    </S.ButtonsWrapper>
-                  ) : (
-                    <IconButton
-                      onPress={onOpenSection}
-                      Icon={() => (
-                        <IconChevronDown
-                          stroke={1.5}
-                          size={theme.iconSizes.sm}
-                          color={theme.colors.textSecondary}
+          <CollapsableContainer isExpanded={isOpen}>
+            <S.PhotosScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            >
+              {photos.map((item, index, array) => (
+                <S.ImageWrapper
+                  key={(index + 1).toString()}
+                  isLastItem={ListSeparators.getIsLastItem(index, array)}
+                >
+                  <S.Header isOpen>
+                    <S.Title>
+                      Fotos adicionais {index + 1}/
+                      {photosQuantityPerSection.complement}
+                    </S.Title>
+                    {isOpen ? (
+                      <S.ButtonsWrapper>
+                        <IconButton
+                          onPress={() =>
+                            handleTakePhoto(uri =>
+                              onSetPhoto({ photoUri: uri, photoIndex: index }),
+                            )
+                          }
+                          Icon={() => (
+                            <IconCamera
+                              stroke={1.5}
+                              size={theme.iconSizes.sm}
+                              color={theme.colors.textSecondary}
+                            />
+                          )}
                         />
-                      )}
+                        <IconButton
+                          disabled={!item.photoUri}
+                          isDisabled={!item.photoUri}
+                          onPress={() => onDeletePhoto({ photoIndex: index })}
+                          Icon={() => (
+                            <IconTrash
+                              stroke={1.5}
+                              size={theme.iconSizes.sm}
+                              color={theme.colors.textSecondary}
+                            />
+                          )}
+                        />
+                      </S.ButtonsWrapper>
+                    ) : (
+                      <IconButton
+                        onPress={onOpenSection}
+                        Icon={() => (
+                          <IconChevronDown
+                            stroke={1.5}
+                            size={theme.iconSizes.sm}
+                            color={theme.colors.textSecondary}
+                          />
+                        )}
+                      />
+                    )}
+                  </S.Header>
+                  {item.photoUri ? (
+                    <S.Image
+                      resizeMode="cover"
+                      resizeMethod="resize"
+                      source={{
+                        uri: item.photoUri,
+                      }}
                     />
+                  ) : (
+                    <TouchableWithoutFeedback
+                      onPress={() =>
+                        handleTakePhoto(uri =>
+                          onSetPhoto({ photoUri: uri, photoIndex: index }),
+                        )
+                      }
+                    >
+                      <S.ImageEmptyWrapper>
+                        <IconCameraOff
+                          stroke={1}
+                          size={theme.layout[28]}
+                          color={theme.colors.textTertiary}
+                        />
+                      </S.ImageEmptyWrapper>
+                    </TouchableWithoutFeedback>
                   )}
-                </S.Header>
-              </S.ImageWrapper>
-            ))}
-          </S.PhotosScrollView>
+                </S.ImageWrapper>
+              ))}
+            </S.PhotosScrollView>
+          </CollapsableContainer>
         ) : (
-          <S.Header isOpen={false}>
-            <S.Title isOpen={false}>Fotos complementares</S.Title>
-            <IconButton
-              onPress={onOpenSection}
-              Icon={() => (
-                <IconChevronDown
-                  stroke={1.5}
-                  size={theme.iconSizes.sm}
-                  color={theme.colors.textSecondary}
-                />
-              )}
-            />
-          </S.Header>
+          <TouchableWithoutFeedback onPress={onOpenSection} disabled={isOpen}>
+            <S.Header isOpen={false}>
+              <S.Title>Fotos adicionais</S.Title>
+              <IconButton
+                onPress={onOpenSection}
+                Icon={() => (
+                  <IconChevronDown
+                    stroke={1.5}
+                    size={theme.iconSizes.sm}
+                    color={theme.colors.textSecondary}
+                  />
+                )}
+              />
+            </S.Header>
+          </TouchableWithoutFeedback>
         )}
       </S.Container>
     );
