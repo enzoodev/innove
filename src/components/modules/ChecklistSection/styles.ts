1bf9badf0cb69ec5f4ec11bdc7f5ba53e2149ed2
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-25 23:31:21 -0300

    refactor: Update ChecklistSection component to include isOpen prop and adjust styling

--- src/components/modules/ChecklistSection/styles.ts
+++ src/components/modules/ChecklistSection/styles.ts
@@ -4,28 +4,39 @@ type TContainerTypeTypeProps = {
   isOpen: boolean;
 };
 
-export const Container = styled.View<TContainerTypeTypeProps>``;
-
-export const Header = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  ${({ theme }) => css`
-    padding: ${theme.layout[2]}px ${theme.layout[2]}px ${theme.layout[2]}px
-      ${theme.layout[4]}px;
+export const Container = styled.View<TContainerTypeTypeProps>`
+  ${({ theme, isOpen }) => css`
+    border-top-width: ${theme.border.width.md}px;
+    border-bottom-width: ${theme.border.width.md}px;
+    border-color: ${theme.colors.cardBorderInner};
+    background-color: ${isOpen
+      ? theme.colors.background
+      : theme.colors.backgroundAlt};
   `};
 `;
 
-export const Title = styled.Text`
-  ${({ theme }) => css`
+export const Header = styled.View<TContainerTypeTypeProps>`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  ${({ theme, isOpen }) => css`
+    gap: ${theme.layout[4]}px;
+    padding: ${theme.layout[isOpen ? 4 : 2]}px ${theme.layout[4]}px;
+  `};
+`;
+
+export const Title = styled.Text<TContainerTypeTypeProps>`
+  flex: 1;
+  ${({ theme, isOpen }) => css`
     font-family: ${theme.fonts.main.semiBold};
-    font-size: ${theme.fontSizes.md}px;
-    color: ${theme.colors.textSecondary};
+    font-size: ${theme.fontSizes.sm}px;
+    color: ${isOpen ? theme.colors.textPrimary : theme.colors.textSecondary};
   `};
 `;
 
 export const QuestionsContainer = styled.View`
   ${({ theme }) => css`
     gap: ${theme.layout[4]}px;
+    padding-bottom: ${theme.layout[4]}px;
   `};
 `;
