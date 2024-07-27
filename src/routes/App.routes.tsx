4c4f9e978965587a7fb3544c644c6ffe9f637781
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-26 22:53:03 -0300

    refactor: Add TakeChecklistPhoto component to AppRoutes

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -4,6 +4,7 @@ import { Settings } from '@/screens/app/Settings';
 import { ExecutionDetails } from '@/screens/app/Home/ExecutionDetails';
 import { UpdatePassword } from '@/screens/app/UpdatePassword';
 import { Checklist } from '@/screens/app/Home/ExecutionDetails/Checklist';
+import { TakeChecklistPhoto } from '@/screens/app/Home/ExecutionDetails/Checklist/TakeChecklistPhoto';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export const AppRoutes = () => {
       <Screen name="Settings" component={Settings} />
       <Screen name="ExecutionDetails" component={ExecutionDetails} />
       <Screen name="Checklist" component={Checklist} />
+      <Screen name="TakeChecklistPhoto" component={TakeChecklistPhoto} />
     </Navigator>
   );
 };
