0b70834f4184698b667000259cb219bfdb0d6ceb
Enzo Damascena <enzodm.dev@gmail.com>
2024-07-30 23:25:51 -0300

    refactor: Update formatLocationAddressLabel to handle optional address parameter

--- src/utils/formatLocationAddressLabel.ts
+++ src/utils/formatLocationAddressLabel.ts
@@ -1,3 +1,15 @@
-export const formatLocationAddressLabel = (address: TLocationAddress) => {
-  return `${address.rua}, ${address.numero}, ${address.bairro}`;
+export const formatLocationAddressLabel = (address?: TLocationAddress) => {
+  if (!address) {
+    return null;
+  }
+
+  const { rua, numero, complemento, cep, bairro, cidade, estado } = address;
+
+  let formattedAddress = `${rua}, ${numero}`;
+  if (complemento) {
+    formattedAddress += `, ${complemento}`;
+  }
+  formattedAddress += ` - ${bairro}, ${cidade} - ${estado}, CEP: ${cep}`;
+
+  return formattedAddress;
 };
