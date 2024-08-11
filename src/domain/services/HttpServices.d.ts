fce32179457e94c4f793be9d6a46198d0e3c20f7
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-11 12:59:53 -0300

    feat: Add IHttpServices interface for managing HTTP services

---
+++ src/domain/services/HttpServices.d.ts
@@ -0,0 +1,7 @@
+interface IHttpServices {
+  get<T = unknown>(params: TRequestConfig): Promise<T>;
+  post<T = unknown>(params: TRequestConfig): Promise<T>;
+  put<T = unknown>(params: TRequestConfig): Promise<T>;
+  delete<T = unknown>(params: TRequestConfig): Promise<T>;
+  patch<T = unknown>(params: TRequestConfig): Promise<T>;
+}
