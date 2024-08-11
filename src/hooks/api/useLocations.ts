ee0551cf0381ed581ebf0245e47e07528fe71f40
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-11 13:01:51 -0300

    refactor: Update imports and dependencies in useLocations.ts

--- src/hooks/api/useLocations.ts
+++ src/hooks/api/useLocations.ts
@@ -1,10 +1,22 @@
-import { LocationRepository } from '@/repositories/api/LocationRepository';
-import { useAppQuery } from '../shared/useAppQuery';
+import { LocationRepository } from '@/infrastructure/repositories/api/LocationRepository';
+import { httpServicesFactory } from '@/infrastructure/factories/httpServicesFactory';
+import { BaseRepository } from '@/infrastructure/repositories/api/shared/BaseRepository';
+
+import { useAuth } from '@/hooks/api/useAuth';
+import { useAppQuery } from '@/hooks/shared/useAppQuery';
+
+import { UrlBuilder } from '@/utils/UrlBuilder';
 
 export const useLocations = (params: TGetLocationsParams) => {
+  const { handleCleanAuth } = useAuth();
+
+  const httpServices = httpServicesFactory({ logout: handleCleanAuth });
+  const baseRepository = new BaseRepository(httpServices, new UrlBuilder());
+  const locationRepository = new LocationRepository(baseRepository);
+
   const { data, isLoading, isPending, isRefetching, refresh, refetch } =
     useAppQuery({
-      request: () => LocationRepository.getLocations(params),
+      request: () => locationRepository.getLocations(params),
       params,
       queryKey: 'locations',
       errorMessage: 'Não foi possível buscar os locais.',
