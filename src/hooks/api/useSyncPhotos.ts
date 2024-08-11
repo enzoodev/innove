2a1785c8a3f810b12ec182b21ce33296cd56c1c1
Enzo Damascena <enzodm.dev@gmail.com>
2024-08-11 13:01:56 -0300

    refactor: Update imports and dependencies in useSyncPhotos.ts

--- src/hooks/api/useSyncPhotos.ts
+++ src/hooks/api/useSyncPhotos.ts
@@ -1,23 +1,39 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { useFocusEffect } from '@react-navigation/native';
 import { useToast } from 'react-native-toast-notifications';
 
+import { ChecklistPhotosStorageRepository } from '@/infrastructure/repositories/local/ChecklistPhotosStorageRepository';
+import { StorageRepository } from '@/infrastructure/repositories/local/shared/StorageRepository';
+import { SyncPhotosRepository } from '@/infrastructure/repositories/api/SyncPhotosRepository';
+import { BaseRepository } from '@/infrastructure/repositories/api/shared/BaseRepository';
+import { httpServicesFactory } from '@/infrastructure/factories/httpServicesFactory';
+
 import { useAuth } from '@/hooks/api/useAuth';
 
-import { SyncPhotosRepository } from '@/repositories/api/SyncPhotosRepository';
-import { ChecklistPhotosStorageRepository } from '@/repositories/local/ChecklistPhotosStorageRepository';
+import { UrlBuilder } from '@/utils/UrlBuilder';
 
 export const useSyncPhotos = () => {
   const toast = useToast();
-  const { userId } = useAuth();
+  const { userId, handleCleanAuth } = useAuth();
+
+  const httpServices = httpServicesFactory({ logout: handleCleanAuth });
+  const baseRepository = new BaseRepository(httpServices, new UrlBuilder());
+  const checklistPhotosStorageRepository = useMemo(
+    () => new ChecklistPhotosStorageRepository(new StorageRepository()),
+    [],
+  );
+  const syncPhotosRepository = new SyncPhotosRepository(
+    baseRepository,
+    checklistPhotosStorageRepository,
+  );
 
   const [hasPhotos, setHasPhotos] = useState(
-    ChecklistPhotosStorageRepository.getHasPhotos(userId),
+    checklistPhotosStorageRepository.getHasPhotos(userId),
   );
 
   const { mutateAsync: syncFn, isPending: isLoadingSync } = useMutation({
-    mutationFn: SyncPhotosRepository.syncAll,
+    mutationFn: syncPhotosRepository.syncAll,
   });
 
   const syncPhotos = useCallback(async () => {
@@ -34,14 +50,14 @@ export const useSyncPhotos = () => {
         placement: 'top',
       });
     } finally {
-      setHasPhotos(ChecklistPhotosStorageRepository.getHasPhotos(userId));
+      setHasPhotos(checklistPhotosStorageRepository.getHasPhotos(userId));
     }
-  }, [syncFn, toast, userId]);
+  }, [checklistPhotosStorageRepository, syncFn, toast, userId]);
 
   useFocusEffect(
     useCallback(() => {
-      setHasPhotos(ChecklistPhotosStorageRepository.getHasPhotos(userId));
-    }, [userId]),
+      setHasPhotos(checklistPhotosStorageRepository.getHasPhotos(userId));
+    }, [checklistPhotosStorageRepository, userId]),
   );
 
   return {
